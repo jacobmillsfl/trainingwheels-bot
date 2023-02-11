55ae0204778a76153313d77d208910d88f7294ef
Luke Collins <lukenatcollins@gmail.com>
2023-02-11 12:47:22 +1100

    Add API call to get recent user submissions

--- src/utils/leetcode_util.py
+++ src/utils/leetcode_util.py
@@ -5,6 +5,7 @@ class LeetcodeUtil:
     GRAPH_URL = "https://leetcode.com/graphql/"
     PROBLEM_URL = "https://leetcode.com/problems/"
     ALL_PROBLEMS_URL = "https://leetcode.com/api/problems/all/"
+    data_limit = 100
 
     def __init__(self):
         pass
@@ -35,3 +36,47 @@ class LeetcodeUtil:
             print(f"Response returned status code : {response.status_code}")
         
         return results
+
+
+    # helper function to build the query specific to the user calling the command
+    @staticmethod
+    def query_builder_recent_stats(leetcode_username: str):
+        query_recent_stats = {
+        "query": """
+            query recentAcSubmissions($username: String!, $limit: Int!) {
+                recentAcSubmissionList(username: $username, limit: $limit) {
+                    id
+                    title
+                    titleSlug
+                    timestamp
+                }
+            }
+        """,
+        "variables": {
+            "username": leetcode_username,
+            "limit": LeetcodeUtil.data_limit
+        }
+        }
+        return query_recent_stats
+
+    # returns a list of recently completed Leetcode challenges as a dictionary.  dict{ id : title}
+    @staticmethod
+    def get_recent_submissions(leetcode_username: str):
+        recent_completions = {}
+        query = LeetcodeUtil.query_builder_recent_stats(leetcode_username)
+        response = requests.get(LeetcodeUtil.GRAPH_URL, json=query)
+        if(response.ok):
+            data = response.json()
+            questions = data['data']['recentAcSubmissionList']
+            for question in questions:
+                recent_completions[question['id']] = question['title']
+        else:
+            print(f"Response returned status code : {response.status_code}")
+
+        return recent_completions
+
+
+    # print(get_recent_submissions("King_of_Cheese"))
+
+
+
