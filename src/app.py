2c205166564735909553d596c53b9594534a34cf
Jacob Mills <jacobmills@adobe.com>
2023-02-16 16:19:12 -0700

    Added argument for updating leetcode questions

--- src/app.py
+++ src/app.py
@@ -16,27 +16,11 @@ if __name__ == "__main__":
     # Program entry point. Parse arguments and launch relevant initialization routines.
 
     config = dotenv_values(".process.env")
-
-    #Note: this is currently a dummy token. Update with official discord bot token once registered.
     DISCORD_AUTH_TOKEN = config.get("DISCORD_AUTH_TOKEN")
-
-    # Example of how to pull all leetcode questions and enter into the database
-    # This will eventually be part of our app initialization routine
-    new_questions = LeetcodeUtil.api_questions_loadall()
-    database = DatabaseUtil(config.get("DATABASE_NAME"))
-    COUNT = database.table_leetcodequestion_insert_many(new_questions)
-    print(f"Inserted {COUNT} new questions into the database")
-
-
-    # Warning: this is just for debugging and will print a lot of data
-    #           if it does, then it worked
-    #our_questions = database.table_leetcodequestion_loadall()
-    #print(our_questions)
-
 
     command_parser = argparse.ArgumentParser(
         prog='TrainingWheels Bot',
-        usage='app.py [--discord]',
+        usage='app.py [--discord] [--update]',
         formatter_class = argparse.RawDescriptionHelpFormatter,
         description='''\
     TrainingWheels Bot.
@@ -48,10 +32,23 @@ if __name__ == "__main__":
 
     command_parser.add_argument('--discord',
         action='store_true',
-        help='run the app in Discord mode, no arguments are needed for this command.')
+        help='Run the app in Discord mode')
+
+    command_parser.add_argument('--update',
+        action='store_true',
+        help='Update the database with new Leetcode questions')
 
     args = command_parser.parse_args()
+
+    if args.update:
+        print("Updating Leetcode questions in our database...")
+        new_questions = LeetcodeUtil.api_questions_loadall()
+        database = DatabaseUtil(config.get("DATABASE_NAME"))
+        count = database.table_leetcodequestion_insert_many(new_questions)
+        print(f"Inserted {count} new questions into the database")
+
     if args.discord:
+        print("Running in Discord mode")
         bot = DiscordUtil(DISCORD_AUTH_TOKEN)
     else:
         bot = StandaloneUtil()
