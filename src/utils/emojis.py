30da7b890c88385b4435e10d2424ce75d0a0084a
= <=>
2023-02-22 18:32:13 -0700

    Added docstr to Emojis class

--- src/utils/emojis.py
+++ src/utils/emojis.py
@@ -4,5 +4,8 @@ Module for Leetcode bot Discord emojis
 from enum import Enum
 
 class Emojis(str, Enum):
+    """
+    An Enum class for Discord emojis
+    """
     check_mark=":white_check_mark:"
     red_x=":x:"
